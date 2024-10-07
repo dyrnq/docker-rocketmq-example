70fbfdafc0b4d9e4802f12bb71a8a0792540b81d
dyrnq <dyrnq@qq.com>
2024-10-07 19:34:25 +0800

    Replace tab

--- compose/launch.sh
+++ compose/launch.sh
@@ -3,12 +3,12 @@
 
 
 for var in "mqnamesrv1" "mqnamesrv2" "mqnamesrv3" "mqbroker1" "mqbroker2" "mqbroker3" ; do
-	for dir in "index" "commitlog" "consumequeue" ; do
-		if [ ! -d persistent/"$var"/store/"$dir" ]; then
-			mkdir -p persistent/"$var"/store/"$dir"
-		fi
-	done
-	
+    for dir in "index" "commitlog" "consumequeue" ; do
+        if [ ! -d persistent/"$var"/store/"$dir" ]; then
+            mkdir -p persistent/"$var"/store/"$dir"
+        fi
+    done
+    
     if [ ! -d persistent/"$var"/logs ]; then
         mkdir -p persistent/"$var"/logs
     fi
@@ -16,19 +16,19 @@ done
 
 DETACHED=${DETACHED:-}
 while [ $# -gt 0 ]; do
-	case "$1" in
-		--detached|-d)
-			DETACHED=1
-			;;
- 		--remove|-r)
-			docker-compose down
-			exit 0
-			;;
-		--*)
-			echo "Illegal option $1"
-			;;
-	esac
-	shift $(( $# > 0 ? 1 : 0 ))
+    case "$1" in
+        --detached|-d)
+            DETACHED=1
+            ;;
+         --remove|-r)
+            docker-compose down
+            exit 0
+            ;;
+        --*)
+            echo "Illegal option $1"
+            ;;
+    esac
+    shift $(( $# > 0 ? 1 : 0 ))
 done
 
 sed -e "s@_BROKERNAME@mqbroker1@g" -e "s@_NAMESRVADDR@mqnamesrv1:9876;mqnamesrv2:9876;mqnamesrv3:9876@" broker.sed > mqbroker1.conf
@@ -37,11 +37,11 @@ sed -e "s@_BROKERNAME@mqbroker3@g" -e "s@_NAMESRVADDR@mqnamesrv1:9876;mqnamesrv2
 
 
 is_detached() {
-	if [ -z "$DETACHED" ]; then
-		return 1
-	else
-		return 0
-	fi
+    if [ -z "$DETACHED" ]; then
+        return 1
+    else
+        return 0
+    fi
 }
 
 if is_detached; then
